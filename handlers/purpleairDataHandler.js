c58973a4a0dc0e4d522fd6886436ade323c10397
kmarsh423 <marzkobie@gmail.com>
2023-04-04 15:13:33 -0400

    added comma

--- handlers/purpleairDataHandler.js
+++ handlers/purpleairDataHandler.js
@@ -29,7 +29,7 @@ const rawData = async () => {
     fetch(`https://api.purpleair.com/v1/sensors?show=${formattedSensorIds}`, {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
         'api_key': '1182661F-CF65-11ED-B6F4-42010A800007'
       }
     })
