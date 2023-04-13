c762c33482d099edf6bc15e2c73a1001d5da36a3
kmarsh423 <marzkobie@gmail.com>
2023-04-12 22:48:15 -0400

    getting value

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -79,7 +79,7 @@ export default function HandleInputForm () {
   const [result, setresult] = useState("");
 
   const handleSensorChange = (e) => {
-    console.log(Object.values(Object.values(e.target.selectedOptions)[0])[1])
+    console.log(Object.values(Object.values(e.target.selectedOptions)[0])[1].defaultValue)
     setInputs((oldValues) => ({
       ...oldValues,
       [e.target.name]: String(e.target.innerHTML.match(/(?<=value=)\d+/g))
