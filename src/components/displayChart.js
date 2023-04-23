3ee0cf8380d3ae61026faaf1f37eb36e71b04d5e
kmarsh423 <marzkobie@gmail.com>
2023-04-23 13:18:36 -0400

    pls work

--- src/components/displayChart.js
+++ src/components/displayChart.js
@@ -264,19 +264,19 @@ export function ShowChart(inputs){
 
         return <div>Error: {error.message}</div>;
 
-    } else if (!isLoaded) {
+    } if (!isLoaded) {
 
         return <div>Loading...</div>;
 
-    } else {
+    } 
 
-        return (
-            <div>
-                <Line options={options} data={data(inputs)} />
-            </div>
-        );
+    return (
+        <div>
+            <Line options={options} data={data(inputs)} />
+        </div>
+    );
 
-    }
+    
 
 }
 
