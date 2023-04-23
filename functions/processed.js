b9d557230490f382ba8c42a058153ef7ab2742a5
kmarsh423 <marzkobie@gmail.com>
2023-04-23 10:12:38 -0400

    logging sensordata

--- functions/processed.js
+++ functions/processed.js
@@ -1,6 +1,7 @@
 var purpleairHandler = require('../handlers/purpleairDataHandler.js');
 exports.handler = async function (event, context) {
   var data = await purpleairHandler.getUpdatedSensorsData();
+  console.log(data)
   return {
     statusCode: 200,
     body: JSON.stringify(data)
